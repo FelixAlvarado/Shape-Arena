cdd93d65bfce580a69935c288b79f8f52102549c
Felix Alvarado <felixalvarado653@gmail.com>
2017-07-16 17:20:48 -0700

    hello

--- game.js
+++ game.js
@@ -30,9 +30,13 @@ var LEFT = 37;
 var UP = 38;
 var DOWN = 40;
 var SPACE = 32;
+var Z = 90;
+var X = 88;
 
 var lineShoot = false;
 var spaceKeyIsDown = false;
+var zIsDown = false;
+var xIsDown = false;
 var moveRight = false;
 var moveLeft = false;
 var moveUp = false;
@@ -66,8 +70,30 @@ window.addEventListener("keydown", function(event)
     {
       lineShoot = true;
       spaceKeyIsDown = true;
+
+    }
+
+    break;
+
+    case Z:
+    if (!moveDown && !moveUp && !moveLeft && !moveRight)
+    {
+      break;
+    }
+    if(!zIsDown)
+    {
+      lineShoot = true;
+      zIsDown = true;
+
     }
     break;
+
+    case X:
+    if (!xIsDown)
+    {
+      lineShoot = true;
+      xIsDown = true;
+    }
 
   }
 }, false);
@@ -93,6 +119,15 @@ window.addEventListener("keyup", function(event)
 
     case SPACE:
     spaceKeyIsDown = false;
+    break;
+
+    case Z:
+    zIsDown = false;
+    break;
+
+    case X:
+    xIsDown = false;
+    break;
 
   }
 },false);
@@ -107,6 +142,7 @@ function removeObject(objectToRemove, array){
 
 function lineFire()
 {
+if (spaceKeyIsDown){
   var missile = {
     width: 12,
     height:12,
@@ -123,26 +159,156 @@ function lineFire()
   if (moveLeft){
     missile.x = line.x - 12;
     missile.y = line.y +  19;
-    missile.vx = -16;
+    missile.vx = -14;
   }
   if (moveRight){
     missile.x = line.x + 10;
     missile.y = line.y +  19;
-      missile.vx = 16;
+      missile.vx = 14;
   }
   if (moveUp){
     missile.x = line.x - 1;
     missile.y = line.y - 12;
-    missile.vy = -16;
+    missile.vy = -14;
   }
   if (moveDown){
     missile.x = line.x - 1;
     missile.y = line.y + 50;
-  missile.vy = 16;
+  missile.vy = 14;
 }
+}
+// line sp attack
 
+if(zIsDown){
+  var missile = {
+    width: 0,
+    height:0,
+    sx: 0,
+    sy: 0,
+    swx: 0,
+    swy: 0,
+    vx:0,
+    vy:0,
+    x: 0,
+    y: 0,
+  }
+
+  if (moveLeft && !moveDown && !moveUp){
+    missile.x = canvas.width;
+    missile.y = line.y + 20;
+    missile.width = 50;
+    missile.height = 10;
+    missile.sx = 50;
+    missile.sy = 0;
+    missile.swx = 50;
+    missile.swy = 10;
+    missile.vx = -10;
+  }
+  if (moveRight  && !moveDown && !moveUp){
+    missile.x = -50;
+    missile.y = line.y + 20;
+    missile.width = 50;
+    missile.height = 10;
+    missile.sx = 50;
+    missile.sy = 0;
+    missile.swx = 50;
+    missile.swy = 10;
+    missile.vx = 10;
+  }
+  if (moveUp && !moveLeft && !moveRight){
+    missile.x = line.x;
+    missile.y = canvas.height;
+    missile.width = 10;
+    missile.height = 50;
+    missile.sx = 0;
+    missile.sy = 0;
+    missile.swx = 10;
+    missile.swy = 50;
+    missile.vy = -10;
+  }
+  if (moveDown && !moveLeft && !moveRight){
+    missile.x = line.x;
+    missile.y = -50;
+    missile.width = 10;
+    missile.height = 50;
+    missile.sx = 0;
+    missile.sy = 0;
+    missile.swx = 10;
+    missile.swy = 50;
+    missile.vy = 10;
+  }
+
+}
+if (spaceKeyIsDown || zIsDown)
+{
   sprites.push(missile);
   missiles.push(missile);
+}
+
+if(xIsDown){
+var missileUp = {
+  width: 10,
+  height: 50,
+  sx: 0,
+  sy: 0,
+  swx: 10,
+  swy: 50,
+  vx:0,
+  vy:-21,
+  x: line.x,
+  y: line.y,
+}
+
+var missileDown = {
+  width: 10,
+  height: 50,
+  sx: 0,
+  sy: 0,
+  swx: 10,
+  swy: 50,
+  vx:0,
+  vy:21,
+  x: line.x,
+  y: line.y,
+}
+
+var missileRight = {
+  width: 50,
+  height: 10,
+  sx: 50,
+  sy: 0,
+  swx: 50,
+  swy: 10,
+  vx:21,
+  vy:0,
+  x: line.x - 25,
+  y: line.y + 20,
+}
+
+var missileLeft = {
+  width: 50,
+  height: 10,
+  sx: 50,
+  sy: 0,
+  swx: 50,
+  swy: 10,
+  vx:-21,
+  vy:0,
+  x: line.x - 25,
+  y: line.y + 20,
+}
+
+sprites.push(missileUp);
+missiles.push(missileUp);
+sprites.push(missileDown);
+missiles.push(missileDown);
+sprites.push(missileLeft);
+missiles.push(missileLeft);
+sprites.push(missileRight);
+missiles.push(missileRight);
+
+}
+
 }
 
 function playGame()
@@ -150,23 +316,23 @@ function playGame()
 //LEFT
 if(moveLeft && !moveRight)
 {
-  line.vx = -8;
+  line.vx = -7;
 
 }
 //RIGHT
 if(!moveLeft && moveRight)
 {
-  line.vx = 8;
+  line.vx = 7;
 }
 //UP
 if(moveUp && !moveDown)
 {
-  line.vy = -8;
+  line.vy = -7;
 }
 //DOWN
 if(moveDown && !moveUp)
 {
-  line.vy = 8;
+  line.vy = 7;
 }
 //zero velocity if no buttons pressed
 if(!moveDown && !moveUp)
@@ -195,7 +361,7 @@ for(var i = 0; i < missiles.length; i++)
   missile.x += missile.vx;
   missile.y += missile.vy;
 
-  if(missile.x < -12 || missile.x > canvas.width || missile.y < -12 || missile.y > canvas.height){
+  if(missile.x < -missile.width || missile.x > canvas.width || missile.y < -missile.height || missile.y > canvas.height){
     removeObject(missile, missiles);
     removeObject(missile,sprites);
     i--;
